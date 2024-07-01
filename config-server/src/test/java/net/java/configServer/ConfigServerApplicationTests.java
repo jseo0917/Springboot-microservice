5100b8c28d6351e317b6da9f36b926bce56c9260
jseo0917 <j.seo0917@gmail.com>
2024-06-30 21:55:52 -0500

    Added zipkin server

---
+++ config-server/src/test/java/net/java/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.java.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
